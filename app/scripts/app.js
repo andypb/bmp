ac83fa8aa9c6665b4ad868868afc72f7930cf162
andypb <andy.birchall@gmail.com>
2016-09-18 18:05:47 +0100

    intial commit after creating with yoeman generator-angular

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name bestmanplanApp
+ * @description
+ * # bestmanplanApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('bestmanplanApp', [
+    'ngResource'
+  ]);
